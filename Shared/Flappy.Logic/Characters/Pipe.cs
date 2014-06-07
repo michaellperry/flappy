532bc1c98ae469cc889dc9756996a16240942729
Michael L Perry <michael@qedcode.com>
2014-06-06 22:44:09 -0500

    Refactor pipe drawing.

--- Shared/Flappy.Logic/Characters/Pipe.cs
+++ Shared/Flappy.Logic/Characters/Pipe.cs
@@ -11,7 +11,12 @@ namespace Flappy.Logic.Characters
 {
     public class Pipe
     {
-        private static readonly Vector2 Origin = new Vector2(27.0f, 12.0f);
+        private const float SegmentWidth = 55.0f;
+        private const float SegmentHeight = 25.0f;
+        private const float SegmentHorizontalCenter = SegmentWidth / 2.0f;
+        private const float SegmentVerticalCenter = SegmentHeight / 2.0f;
+        private static readonly Vector2 Origin = new Vector2(SegmentHorizontalCenter, SegmentVerticalCenter);
+        private const int GapSize = 7;
 
         private Texture2D _capTop;
         private Texture2D _capBottom;
@@ -27,27 +32,33 @@ namespace Flappy.Logic.Characters
         public void Draw(SpriteBatch spriteBatch, Rectangle bounds, Camera camera)
         {
             int gapStart = 5;
-            int gapEnd = 12;
+            int gapEnd = gapStart + GapSize;
 
-            int segmentCount = (int)Math.Ceiling(bounds.Height / 25.0f);
-            for (int segmentIndex = 0; segmentIndex < segmentCount; ++segmentIndex)
+            int segmentCount = (int)Math.Ceiling(bounds.Height / SegmentHeight);
+            for (int segmentIndex = 0; segmentIndex < gapStart; ++segmentIndex)
             {
-                if (gapStart < segmentIndex && segmentIndex < gapEnd)
-                    continue;
-
-                Vector2 position = new Vector2(800.0f, 12.0f + segmentIndex * 25.0f);
-
-                Texture2D image;
-                if (segmentIndex == gapStart)
-                    image = _capBottom;
-                else if (segmentIndex == gapEnd)
-                    image = _capTop;
-                else
-                    image = _segment;
-                spriteBatch.Draw(image,
-                    position: position - camera.Position,
-                    origin: Origin);
+                DrawSegment(spriteBatch, camera, segmentIndex, _segment);
             }
+            DrawSegment(spriteBatch, camera, gapStart, _capBottom);
+            DrawSegment(spriteBatch, camera, gapEnd, _capTop);
+            for (int segmentIndex = gapEnd + 1; segmentIndex < segmentCount; ++segmentIndex)
+            {
+                DrawSegment(spriteBatch, camera, segmentIndex, _segment);
+            }
+        }
+
+        private static void DrawSegment(
+            SpriteBatch spriteBatch,
+            Camera camera,
+            int segmentIndex,
+            Texture2D image)
+        {
+            Vector2 position = new Vector2(800.0f,
+                SegmentVerticalCenter + segmentIndex * SegmentHeight);
+
+            spriteBatch.Draw(image,
+                position: position - camera.Position,
+                origin: Origin);
         }
     }
 }
